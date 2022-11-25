2977f7384c698e2e3112784a9e08555b0df26364
vaylette <118726848+vaylette@users.noreply.github.com>
2022-11-25 11:51:12 +0300

    modified nothing

--- smartstudio/resources/views/contact_email.blade.php
+++
@@ -1,9 +0,0 @@
-<h2>Hello Admin,</h2>
-You received an email from : {{ $name }}
-Here are the details:
-<b>Name:</b> {{ $name }}
-<b>Email:</b> {{ $email }}
-<b>Phone Number:</b> {{ $phone_number }}
-<b>Subject:</b> {{ $subject }}
-<b>Message:</b> {{ $user_message }}
-Thank You
